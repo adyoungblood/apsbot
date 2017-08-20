92f488d4ffb77709dded42494671f627e93fa6ce
Alexander Youngblood <arghalexander3000@gmail.com>
2017-08-20 15:16:50 -0700

    d

--- apsbot/chat.py
+++ apsbot/chat.py
@@ -2,13 +2,13 @@ import discord
 import asyncio
 import time
 
-@apsbot.base.prefunc
+@base.prefunc
 async def check_shush(client, message):
 	with open('config.json', 'r') as config:
 		if message.author == config['shushed']:
 			client.delete_message(message)
 
-@apsbot.base.apsfunc
+@base.apsfunc
 async def shush(client, message, votee):
 	await client.send_message(message.channel, "Starting a vote to shush {}. Respond with either 'yae' or 'nae' within the next 30 seconds to cast your vote.".format(votee.name))
 	vote_start = time.datetime.now
