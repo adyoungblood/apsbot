504ee32a27defae9c1c9b9890d7a58767ff868c2
Alexander Youngblood <arghalexander3000@gmail.com>
2017-08-20 15:28:59 -0700

    all main functions

--- apsbot/chat.py
+++ apsbot/chat.py
@@ -63,3 +63,68 @@ async def shush(client, message):
 		await client.send_message(message.channel, 'The vote has failed.')
 	with open('config.json', 'w') as outfile:
 		json.dump(configtxt, outfile)
+
+@base.apsfunc
+async def unshush(client, message):
+	shushuser = configtxt['shushed']
+	await client.send_typing(message.channel)
+	await asyncio.sleep(0.5)
+	vote_start = await client.send_message(message.channel, "Starting a vote to unshush {}. Respond with either 'aye' or 'nae' within the next 30 seconds to cast your vote.".format(message.server.get_member(shushuser)))
+	voted = []
+	y = 0
+	await asyncio.sleep(15)
+	async for vote in client.logs_from(message.channel, limit=100, after=vote_start):
+		if vote.author in voted or vote.author.id == shushuser:
+			continue
+		else:
+			if check_vote(vote.content) is None:
+				continue
+			elif check_vote(vote.content):
+				y += 1
+				print('User {} voted aye.'.format(vote.author))
+				voted.append(vote.author)
+			else:
+				print('User {} voted nae.'.format(vote.author))
+				voted.append(vote.author)
+	in_channel = [m for m in message.server.members if m.status == discord.Status.online and message.channel.permissions_for(m).send_messages and not m.bot]
+	requirement = 0
+	if len(in_channel) == 2 or len(in_channel) == 1:
+		requirement = 1
+	else:
+		requirement = math.ceil(len(in_channel) / 2)
+		#requirement = 1
+	if y >= requirement:
+		await asyncio.sleep(1)
+		await client.send_message(message.channel, 'The vote has passed. The user {} has been unshushed. Use !shush (user) to redo this.'.format(message.server.get_member(shushuser)))
+		configtxt['shushed'] = ''
+	else:
+		await client.send_typing(message.channel)
+		await asyncio.sleep(1)
+		await client.send_message(message.channel, 'The vote has failed.')
+	with open('config.json', 'w') as outfile:
+		json.dump(configtxt, outfile)
+
+@base.apsfunc
+async def isshushed(client, message):
+	if configtxt['shushed'] == '':
+			await client.send_message(message.channel, 'No one is currently shushed.')
+		else:
+			await client.send_message(message.channel, 'The user {} is currently shushed.'.format(message.server.get_member(configtxt['shushed'])))
+
+@base.apsfunc
+async def youthere(client, message):
+	await client.send_message(message.channel, 'Yes.')
+	
+@base.apsfunc
+async def update(client, message):
+	if message.author == message.server.get_member('283414992752082945'):
+		print('Closing to update')
+		os.system('gitpull.bat')
+		sys.exit()
+		
+@base.apsfunc
+async def off(client, message):
+	if message.author == message.server.get_member('283414992752082945'):
+		await client.close()
+
+		
