fe2bc81bdee3271be5085c0d4d2c4d681311c083
Alexander Youngblood <arghalexander3000@gmail.com>
2017-08-20 21:38:41 -0700

    help command

--- apsbot/help.py
+++ apsbot/help.py
@@ -1,2 +1,14 @@
 import discord
 import asyncio
+
+from apsbot import base
+
+@base.apsfunc
+async def help(client, message):
+	await client.send_message(message.channel, 'These are my commands: ')
+	for func in apsbot.base.pres:
+		await client.send_message(message.channel, func)
+	for func in apsbot.base.functions:
+		await client.send_message(message.channel, func)
+	for func in apsbot.base.posts:
+		await client.send_message(message.channel, func)
