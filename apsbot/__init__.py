b16f738e6a91c2563164cfd8cdafaa625a4cfaad
Alexander Youngblood <arghalexander3000@gmail.com>
2017-08-22 23:07:49 +0000

    better gunfacts

--- apsbot/__init__.py
+++ apsbot/__init__.py
@@ -1,5 +1,6 @@
 import apsbot.chat
 import apsbot.help
+import apsbot.gun_facts
 
 import discord
 import asyncio
--- apsbot/gun_facts.py
+++ apsbot/gun_facts.py
@@ -4,9 +4,10 @@ from apsbot import base
 
 @base.apsfunc
 async def gunfact(client, message):
-	"""**{0}gunfact**
+	'''**{0}gunfact**
 	Provides a helpful fact about a random gun, usually from the WW1/2 era.
 	Facts courtesy of from Louis Leon.
-	*Example: '{0}gunfact'*"""
+	*Example: '{0}gunfact'*'''
 	with open('gunfacts.txt', 'r') as gunfacts:
-		await client.send_message(message.channel, "Here's a little fact: {}".format(choice(gunfacts)))
+		facts = gunfacts.read()
+		await client.send_message(message.channel, "Here's a little fact: {}".format(choice(facts)))
