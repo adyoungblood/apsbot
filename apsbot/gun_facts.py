a7792d7be88d39a25ca40b4be906e9dea559b810
Alexander Youngblood <arghalexander3000@gmail.com>
2017-08-22 15:43:36 -0700

    picks a fact from txt

--- apsbot/gun_facts.py
+++ apsbot/gun_facts.py
@@ -2,3 +2,11 @@ from random import choice
 
 from apsbot import base
 
+@base.apsfunc
+async def gunfact(client, message):
+	"""**{0}gunfact**
+	Provides a helpful fact about a random gun, usually from the WW1/2 era.
+	Facts recevied from Louis Leon.
+	*Example: '{0}gunfact'*"""
+	with open('gunfacts.txt', 'r') as gunfacts:
+		await client.send_message(message.channel, choice(gunfacts))
