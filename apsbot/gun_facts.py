9619a9d073f981cda0d641e9aed29d6ebc866a2f
Alexander Youngblood <arghalexander3000@gmail.com>
2017-08-28 07:40:43 -0700

    adding gunfacts func
    only for me

--- apsbot/gun_facts.py
+++ apsbot/gun_facts.py
@@ -9,3 +9,9 @@ async def gunfact(client, message):
 	Facts courtesy of from Louis Leon.
 	*Example: '{0}gunfact'*'''
 	await client.send_message(message.channel, "Here's a little fact: {}".format(choice(base.facts)))
+	
+	
+@base.apsfunc
+async def addgunfact(client, message):
+	if message.author.id == '283414992752082945':
+		base.facts.append(message.content.split[1])
