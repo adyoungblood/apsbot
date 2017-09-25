2997d60e1de61b34b40908e50820b410b244a66d
Alexander Youngblood <arghalexander3000@gmail.com>
2017-09-25 16:02:52 -0700

    more testinating

--- apsbot/admin.py
+++ apsbot/admin.py
@@ -7,6 +7,8 @@ async def changecolor(client, message):
 	Changes the color of the specified role.
 	I can't change roles higher than my own,
 	nor roles higher than yours.
+	When picking roles, if a search matches two or more roles, 
+	it will choose the first one that is returned.
 	*Example: '{0}changecolor myrole green'*'''
 	await client.send_message(message.channel, "Choose a role to edit:")
 	for role in message.author.roles:
@@ -14,16 +16,18 @@ async def changecolor(client, message):
 			continue
 		else:
 			await client.send_message(message.channel, role.name)
-	choice = await client.wait_for_message(author=message.author, timeout=30)
-	rolechoice = [s for s in message.author.roles if choice.content.lower() in s.name.lower()]
-	print(rolechoice)
-	'''
-	if choice > message.author.top_role or choice == 'everyone':
+	while True:
+		choice = await client.wait_for_message(author=message.author, timeout=30)
+		rolechoice = [s for s in message.author.roles if choice.content.lower() in s.name.lower()]
+		if rolechoice:
+			break
+	if rolechoice >= message.author.top_role:
 		await client.send_message(message.channel, "I can't let you do that.")
 		return
 	else:
-		await client.send_message(message.channel, "Choose color to change {}'s color to.".format(choice))
-		newcolor = await client.wait_for_message(author=message.author, timeout=30)
-		try:
-			await client.edit_role(message.server, message.author.role, newcolor)
-	'''
+		await client.send_message(message.channel, "Choose color to change {}'s color to.".format(rolechoice.name))
+		while True:
+			newcolor = await client.wait_for_message(author=message.author, timeout=30)
+			await client.edit_role(message.server, message.author.role, color=discord.Color.{}().format(newcolor))
+			break
+				
