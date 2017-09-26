3ff30a99d2fdf5b7c89ef9200a7fe2ed91a9122e
Alexander Youngblood <arghalexander3000@gmail.com>
2017-09-25 17:36:08 -0700

    no bots

--- apsbot/admin.py
+++ apsbot/admin.py
@@ -18,7 +18,7 @@ async def changecolor(client, message):
 	await client.send_message(message.channel, "Choose a role to edit:")
 	authorroles = []
 	for role in message.server.roles:
-		if role <= message.author.top_role:
+		if role <= message.author.top_role and !role.managed:
 			authorroles.append(role)
 	for role in authorroles:
 		print(role.name)
